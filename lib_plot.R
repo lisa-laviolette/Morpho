6d6be509582601cd2ab15088f4f6815dc23b8bf5
Jean-Olivier Irisson <irisson@normalesup.org>
2022-06-16 09:12:30 +0000

    Add alias for date_x constant

--- lib_plot.R
+++ lib_plot.R
@@ -2,3 +2,4 @@ library("ggplot2")
 
 x_year <- list(scale_x_date(date_breaks="1 year", date_labels="%Y"), theme(panel.grid.minor.x=element_blank()))
 x_season <- scale_x_date(date_breaks="2 months", date_labels="%b")
+date_x <- x_season
