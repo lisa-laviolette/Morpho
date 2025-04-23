b3df71eec50db18a5f453b526b624c2fe7e8ddfd
lisa-laviolette <Lisa.laviolette@student.uclouvain.be>
2025-04-23 15:31:48 +0200

    Update 5.compute_diversity_indices.R

--- 5.compute_diversity_indices.R
+++ 5.compute_diversity_indices.R
@@ -16,11 +16,11 @@ source("lib_functional_diversity.R")
 # for taxonomic diversity indexes
 library("vegan")
 
-load("4_incl2020.Rdata")
+load("4_incl2020.Rdata")  # données avec morphs et coordonnées PCA
 
-## Compute functional diversity indexes ----
+## ÉTAPE 1 : INDICES DE DIVERSITÉ MORPHOLOGIQUE ----
 
-# weights matrix: date x morphs
+#  Matrice de poids (abondance par morph),lignes = dates, colonnes = morphs
 weights <- z |>
   # concentration per date per morph
   group_by(date, morph_nb) |> summarise(conc=sum(conc)) |>
@@ -32,23 +32,25 @@ rownames(weights) <- as.character(weights$date)
 weights <- select(weights, -date)
 weights <- as.matrix(weights)
 
-# morphological space matrix: morphs x axes
+# Matrice des positions morphologiques (morphs dans PCA) morphological space matrix: morphs x axes
 space <- centers |>select(Dim.1:Dim.4) |> as.matrix()
 rownames(space) <- centers$morph_nb
 
-# compute index
+# Calcul index
 fd <- multidimFD4(space, weights, verb=TRUE)
 
-# convert result into data.frame
+# Formatage des résultats
+# MRic = Richesse morphologique, MDiv = Divergence, MEve = Équitabilité
+# Nb_sp = nb de morphs présents ; Tot_weight = concentration totale
 d <- as.data.frame(fd)
 d$date <- row.names(d) |> as.Date()
 d <- as.tibble(d) |>
   select(date, nb_morphs=Nb_sp, conc=Tot_weight, MRic=FRic, MDiv=FDiv, MEve=FEve)
+# MRic, MDiv, MEve = indices morphologiques Ajout de la concentration totale et du nombre de morphs actifs
 
+## ÉTAPE 2 : INDICES DE DIVERSITÉ TAXONOMIQUE ----
 
-## Compute taxonomic diversity indexes ----
-
-# compute community matrix: date x taxon
+# Matrice communauté : lignes = dates, colonnes = taxons
 comm <- z |>
   group_by(date, taxon) |> summarise(conc=sum(conc)) |>
   spread(key=taxon, value=conc, fill=0) |>
@@ -57,23 +59,26 @@ rownames(comm) <- as.character(comm$date)
 comm <- select(comm, -date)
 comm <- as.matrix(comm)
 
-# compute taxonomic diversity indexes
+# Calcul des indices taxonomiques
+# TRic = richesse spécifique, TShannon = Shannon, TPielou = équitabilité
 d <- mutate(d,
-  TRic = specnumber(comm),
-  TShannon = diversity(comm, index="shannon"),
-  TPielou = TShannon / log(TRic)
+  TRic = specnumber(comm), # Richesse spécifique (nombre de taxons)
+  TShannon = diversity(comm, index="shannon"), # Indice de Shannon
+  TPielou = TShannon / log(TRic) # Équitabilité de Pielou
   )
 
 
-## Inspect indexes ----
+## Inspect indexes 
 
-# plot time series
+# Séries temporelles de tous les indices (morpho + taxo)
 dt <- gather(d, key="index", val="val", conc:TPielou)
 dt$index <- factor(dt$index, levels=unique(dt$index))
 ggplot(dt) +
   geom_path(aes(x=date, y=val)) + facet_grid(index~., scales="free_y")
 
 # plot only morphological indices
+# Séries morphologiques uniquement avec lissage par moyenne glissante
+
 d |> rename(Richness=MRic, Divergence=MDiv, Evenness=MEve) |>
   gather(key="index", val="val", Richness:Evenness) |>
   mutate(index=factor(index, levels=unique(index))) |>
@@ -87,7 +92,8 @@ ggplot() +
 ggsave("plots/morpho_indexes_series_incl2020_coponly.pdf", width=8, height=5)
 
 
-# compare taxo and morphological indexes
+## ÉTAPE 4 : COMPARAISON MORPHO VS TAXO ----
+#Richesse taxo vs morpho Shannon (taxo) vs Divergence (morpho) Pielou (taxo) vs Équitabilité (morpho)
 ggplot(d) +
   geom_point(aes(x=TRic/max(TRic), y=MRic), alpha=0.5, shape=16) +
   geom_abline(slope=1, intercept=0)
@@ -104,9 +110,10 @@ ggplot(d) +
 cor.test(d$TPielou, d$MEve, method="spearman")
 
 
-#add also mean coordinates along PCA axes to df
+# Ajout des coordonnées moyennes sur la PCA axes to df
+# Moyenne, écart-type et quartiles des coordonnées PCA par date
 ax<- z |> group_by(date) |> summarise_at(vars(Dim.1:Dim.4), funs(mean, sd, quantile(., 0.25), quantile(., 0.75)))
 colnames(ax)<-c("date", "Dim.1_mean", "Dim.2_mean", "Dim.3_mean", "Dim.4_mean", "Dim.1_sd", "Dim.2_sd", "Dim.3_sd", "Dim.4_sd", "Dim.1_q25", "Dim.2_q25", "Dim.3_q25", "Dim.4_q25", "Dim.1_q75", "Dim.2_q75", "Dim.3_q75", "Dim.4_q75")
 d<- left_join(d, ax, by="date")
-
+## SAUVEGARDE ----
 save(d, file="5_incl2020_coponly.Rdata")
